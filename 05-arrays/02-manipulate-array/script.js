506c06c52efd1ceee7e3e18264feb407eca3fe9b
Reinout <reinout.de.bleser@gmail.com>
2021-10-01 10:27:23 +0200

    exercices 1-9 done, optionals not complete

---
+++ 05-arrays/02-manipulate-array/script.js
@@ -0,0 +1,30 @@
+/* becode/javascript
+ *
+ * /05-arrays/02-manipulate-array/script.js - 5.2: manipulation de tableau
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+document.getElementById("run").addEventListener("click", function () {
+
+    var fruits = [
+        "pomme",
+        "poire",
+        "fraise",
+        "tomate",
+        "orange",
+        "mandarine",
+        "durian",
+        "pêche",
+        "raisin",
+        "cerise",
+    ];
+    fruits.splice(0, 1, "banana");
+    let a = fruits.length;
+    console.log(a);
+    fruits.splice(9, 1, "kiwi");
+    console.log(fruits);
+})
---
+++ 05-arrays/08-array-includes/script.js
@@ -0,0 +1,39 @@
+/* becode/javascript
+ *
+ * /05-arrays/08-array-includes/script.js - 5.8: présence dans un tableau
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    let fruits = [
+        "apple",
+        "perry",
+        "strawberry",
+        "tomato",
+        "kiwi",
+        "banana",
+        "orange",
+        "mandarin",
+        "durian",
+        "peach",
+        "grapes",
+        "cherry",
+    ];
+
+    document.getElementById("run").addEventListener("click", function (){
+let aDay = fruits.includes("apple")
+if (true) {
+    console.log("yes");
+}
+else {
+    console.log("no");
+}
+
+    ; })
+})();
