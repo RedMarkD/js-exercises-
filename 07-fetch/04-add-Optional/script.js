8acd292dc74969919b5cdc2503b4de9357420863
Reinout <reinout.de.bleser@gmail.com>
2021-10-08 10:02:57 +0200

    optionals from 7 also done

--- 07-fetch/04-add-Optional/script.js
+++ 07-fetch/04-add-Optional/script.js
@@ -8,7 +8,9 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
-//When you click on the button, retrieve the values of the form and create the new hero in the API.
+//When you click on the button, retrieve the values of the form check, split up and create array;
+// and create the new hero in the API. how?
+
    // Each field must be filled in (it is up to you to manage).
 
 //Display the result of all your heroes in the console.
@@ -17,29 +19,39 @@
 
    // You do not have to update the JSON file, it is oké that when you refresh the page you lose the newly added heroes.
 
-async function fetchHeroesJSON() {
+(async() => {
+
+
+document.querySelectorAll("input").required = true;
+//make 3 id's queryselector
+
     const response = await fetch('../../_shared/api.json');
-    // fetch the data
     const data = await response.json();
-    let heroesArr = data.heroes;
+    let heroesArr = await data.heroes;
+
+
+document.getElementById("run").addEventListener("click", () => {
+    let length = heroesArr.length;
     console.log(heroesArr);
-    for (let i = 0; i < heroesArr.length; i++) {
-        {
-            // compare input with array filter object data from array
-            let hero = document.getElementById("tpl-hero").content.cloneNode(true);
-            let target = document.getElementById("target");
-            //clone the template node, append to ol
-            hero.querySelector(".name").innerText = heroesArr[i].name;
-            //add data from array to nametags
-            hero.querySelector(".alter-ego").innerText = heroesArr[i].alterEgo;
-            hero.querySelector(".powers").innerText = heroesArr[i].abilities;
-            target.appendChild(hero);
-        }
-
+    let heroName = document.getElementById("hero-name").value;
+    let heroEgo = document.getElementById("hero-alter-ego").value;
+    let heroPower = document.getElementById("hero-powers").value.split(",");
+    let i = length
+    let newHero = {
+        id: i,
+        name: heroName,
+        alterEgo: heroEgo,
+        abilities: heroPower,
     }
+    console.log(newHero);
+    heroesArr.push(newHero);
+    console.log(heroesArr);
+})
+})();
 
-}
-//const heroID =  await heroes.id[heroNr];
-//console.log(heroID);
-document.getElementById("run").addEventListener("click", fetchHeroesJSON);
-// fetched heroes
+
+
+
+
+
+// added heroes
--- 07-fetch/05-delete-Optional/script.js
+++ 07-fetch/05-delete-Optional/script.js
@@ -9,6 +9,32 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(() => {
-    // your code here
+(async() => {
+    const response = await fetch('../../_shared/api.json');
+    //fetch first
+    const data = await response.json();
+    //get the heroes from api through response.
+    let heroesArr = await data.heroes;
+    //define the array we'll need to use.
+
+
+    document.getElementById("run").addEventListener("click", () => {
+        let id = document.getElementById("hero-id").value;
+        //get input we can use from the user after they click the button
+        let iD= parseInt(id)
+        //value of id in API is a number, so need to convert the value to a number. with parseInt
+        let ide = iD - 1
+        //because the id's are not the same as the array identifiers we need to select one earlier.
+        for (let i = 0; i<5; i++)
+        //let's loop through the array with a very limited boolean in an if function
+        {
+            if (iD === heroesArr[ide].id) {
+                //we want the user input to be equal to the array id in value. structure array[x].id needed.
+                heroesArr.splice(ide, 1);
+                //remove the correct object.
+            }
+        }
+        console.log(heroesArr);
+        //check if the array is gone.
+    })
 })();
